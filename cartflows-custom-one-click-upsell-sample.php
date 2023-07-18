96a5c060fa5301e3a1d3b3c210241f8765b84ccc
Sarang Shahane <sarangshahane321@gmail.com>
2023-07-18 13:19:19 +0530

    ReadMe file updated and function comments modified

--- cartflows-custom-one-click-upsell-sample.php
+++ cartflows-custom-one-click-upsell-sample.php
@@ -4,8 +4,8 @@
  * Plugin URI: https://cartflows.com/
  * Description: This plugin will give you more information about how to add the custom integration of your own payment gateway for the Upsell & Downsell.
  * Version: 1.0.0
- * Author: Sarang Shahane
- * Author URI: http://resume.sarangshahane.in/
+ * Author: CartFlows Inc
+ * Author URI: https://cartflows.com/
  * Text Domain: cartflows-cgis
  *
  * @package cartflows-cgis
